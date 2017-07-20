72ccca32087e4b319b7f0d8c8dd3ae7d2e39407c
webmax0110 <webmax0110.dev@gmail.com>
2017-07-20 14:49:25 +0200

    Timeapp

---
+++ server/config/express.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var express = require('express');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var config = require('./environment');
+var passport = require('passport');
+
+module.exports = function(app) {
+  var env = app.get('env');
+
+  app.set('views', config.root + '/server/views');
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cookieParser());
+  app.use(passport.initialize());
+
+  if ('production' === env) {
+    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(express.static(path.join(config.root, 'public')));
+    app.set('appPath', config.root + '/public');
+    app.use(morgan('dev'));
+
+    // CORS middleware (for production only)
+    app.use(function(req, res, next) {
+      // CORS header
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+
+      // Set custom headers for CORS
+      res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+      if (req.method == 'OPTIONS') {
+        res.status(200).end();
+      } else {
+        next();
+      }
+    });
+  } else  {
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with,authorization123456,accept');
+      next();
+    });
+
+    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', 'client');
+    app.use(morgan('dev'));
+  }
+};
